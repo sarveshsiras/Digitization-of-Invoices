24bc43cb78a5e35aacca54a10651aa13d7935736
Saurabh Rajesh Kshirsagar <kshirsagarsrk@gmail.com>
2021-04-22 00:23:41 +0530

    Added s3 image download function

---
+++ s3_image_download.py
@@ -0,0 +1,11 @@
+import numpy as np
+import urllib.request
+import cv2
+
+
+def url_to_image(url):
+	resp = urllib.request.urlopen(url)
+	image = np.asarray(bytearray(resp.read()), dtype="uint8")
+	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+	# return the image
+	return image
