a166a48121b24ef0c79f1088693d7c9e8d15b181
SarveshSiras <sarveshsiras@gmail.com>
2021-04-21 21:31:15 +0530

    added crop image function

--- api_result.py
+++ api_result.py
@@ -5,21 +5,22 @@ from server_request import dispatch_request
 
 def get_table(img_path):
     print("Start the process")
-    returned_coordinates = dispatch_request("DEPLOYED_MODEL_URL", img_path)
+    returned_coordinates = [209.32167053222656, 445.782958984375, 757.7854614257812, 655.8165893554688]
+    # returned_coordinates = dispatch_request("DEPLOYED_MODEL_URL", img_path)
     print(returned_coordinates)
-    img = Image.open(img_path)
+    # img = Image.open(img_path)
     oimg = cv2.imread(img_path)
     cv2.rectangle(oimg, (int(returned_coordinates[0]), int(returned_coordinates[1])), (int(returned_coordinates[2]), int(returned_coordinates[3])), (0, 255, 0), 3)
     cv2.imshow('detected table', oimg)
     cv2.waitKey()
-    cropped_image = img.crop(returned_coordinates)
-    #open_cv_image = np.array(cropped_image)
-    cropped_image.save("sample.png")
-    open_cv_image = cv2.imread("sample.png")
-    # Convert RGB to BGR
-    # open_cv_image = open_cv_image[:, :, ::-1].copy()
-    #open_cv_image = cv2.imread(img_path)
-    return open_cv_image,returned_coordinates
+    return returned_coordinates
+
+def crop_image(img_url, returned_coordinates):
+    img = cv2.imread(img_url)
+    crop_img = img[round(returned_coordinates[1]):round(returned_coordinates[3]), round(returned_coordinates[0]):round(returned_coordinates[2])]
+    cv2.imshow("cropped", crop_img)
+    cv2.waitKey(0)
+    return crop_img
 
 def get_image(img_path,returned_coordinates):
     x1_coordinate = int(returned_coordinates[0])
