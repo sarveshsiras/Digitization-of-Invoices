a796ec1eb81ce90c1b4e29202f1918fec9ef6819
SarveshSiras <sarveshsiras@gmail.com>
2021-05-05 20:33:04 +0530

    added flask utilities for frontend

--- app.py
+++ app.py
@@ -191,3 +191,160 @@ def upload_file():
     else:
         return render_template('upload.html')
 
+
+def extract_file_names(name):
+    rev = name.split("___")[1]
+    return rev
+
+@app.route('/crop', methods=['GET', 'POST'])
+def get_table():
+    URL = current_user.uploaded_image_urls[-1]
+    returned_coordinates = interface.get_table_coordinates_interface(URL)
+    return render_template('crop.html', filename=URL, returned_coordinates = returned_coordinates)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    uploaded_url_list = current_user.uploaded_image_urls
+    print(uploaded_url_list)
+    number_of_invoices = len(uploaded_url_list)
+    file_name_list = []
+    for url in uploaded_url_list:
+        file_name_list.append(extract_file_names(url))
+    print(number_of_invoices)
+    print(file_name_list)
+    processed_excel_urls = current_user.processed_excel_urls
+    uploaded_urls = zip(uploaded_url_list,file_name_list,processed_excel_urls)
+    return render_template('dashboard.html', segment='index', no_of_invoices_processed = number_of_invoices, url_list = uploaded_urls)
+
+
+@app.route('/profile' ,methods = ['GET','POST'])
+@login_required
+def profile():
+    number_of_invoices = len(current_user.uploaded_image_urls)
+    if(request.method == 'GET'):
+        return render_template('profile.html',Number_of_invoices = number_of_invoices, pass_msg = "")
+    else:
+        password_1 = request.form['Password_1']
+        password_2 = request.form['Password_2']
+        if(password_1 != password_2):
+            return render_template('profile.html',Number_of_invoices = number_of_invoices, pass_msg_1 = "Passwords don't match. Please Reenter",pass_msg_2 = "")
+
+        else:
+            hashpass = generate_password_hash(password_1, method='sha256')
+            current_user.password = hashpass
+            current_user.save()
+            return render_template('profile.html',Number_of_invoices = number_of_invoices, pass_msg_1 = "",pass_msg_2 = "Password Saved Successfully")
+
+@app.route('/')
+def default():
+    return redirect(url_for('login'))
+
+@app.route("/login",methods = ['GET','POST'])
+def login():
+    login_form = LoginForm(request.form)
+    if 'login' in request.form:
+        
+        # read form data
+        username = request.form['username']
+        password = request.form['password']
+
+        #Locate user
+        logged_in_user = user_data.objects(username=username).first()
+
+        # Check the password
+        if logged_in_user and check_password_hash(logged_in_user.password, password):
+            print("YES")
+            login_user(logged_in_user)
+            return redirect(url_for('default'))
+
+        # Something (user or pass) is not ok
+        return render_template( 'accounts/login.html', msg='Wrong Username or password', form=login_form)
+
+    if not current_user.is_authenticated:
+        return render_template( 'accounts/login.html',
+                                form=login_form)
+    
+    return redirect(url_for('dashboard'))
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = LoginForm(request.form)
+    create_account_form = CreateAccountForm(request.form)
+    if 'register' in request.form:
+
+        username  = request.form['username']
+        email     = request.form['email'   ]
+        password  = request.form['password']
+
+
+        if form.validate():
+            # Check usename exists
+            existing_username = user_data.objects(username=username).first()
+            #print(existing_username.username)
+            if existing_username:
+                return render_template( 'accounts/register.html', 
+                                        msg='Username already registered',
+                                        success=False,
+                                        form=create_account_form)
+            
+            existing_mail = user_data.objects(email=email).first()
+            if existing_mail:
+                return render_template( 'accounts/register.html', 
+                                        msg='Email already registered',
+                                        success=False,
+                                        form=create_account_form)
+
+
+            else:
+                hashpass = generate_password_hash(password, method='sha256')
+                new_user = user_data(username = username,email = email,password = hashpass)
+                new_user.save()
+                login_user(new_user)
+                return render_template( 'accounts/register.html', 
+                                msg='User created please <a href="/login">login</a>', 
+                                success=True,
+                                form=create_account_form) 
+
+    else:
+        return render_template( 'accounts/register.html', form=create_account_form)
+
+
+@app.route('/display', methods=['POST'])
+@login_required
+def show():
+    if request.method == 'POST':
+        x1 = request.form['X1']
+        y1 = request.form['Y1']
+        x2 = request.form['X2']
+        y2 = request.form['Y2']
+        print(x1, y1, x2, y2)
+        returned_coordinates = [x1, y1, x2, y2]
+        URL = current_user.uploaded_image_urls[-1]
+        detected_table = interface.crop_image_interface(URL, returned_coordinates)
+        text_data, non_tabular_data = process_invoice(URL, detected_table, returned_coordinates)
+        return render_template("invoice_data.html",invoice_url = URL, data = text_data, non_tabular_data = non_tabular_data)
+
+@app.route('/render', methods=['POST'])
+@login_required
+def get_names():
+    if request.method == 'POST':
+        boxes = request.get_json()
+        print(boxes['boxes'][0].values())
+
+    return redirect(url_for("index"))
+
+@app.route('/logout')
+@login_required
+def logout():
+    login_form = LoginForm(request.form)
+    logout_user()
+    print("WORKS")
+    return redirect(url_for('login'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user_data.objects(pk=user_id).first()
+
+if __name__ == "__main__":
+    app.run(debug=True)
